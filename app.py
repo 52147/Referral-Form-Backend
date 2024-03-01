8c22c9c5360fb17507b163349f428b2ad197d21a
Shou-Tzu Han <debra4117@gmail.com>
2024-02-29 20:52:35 -0600

    update

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from controller import app
-
+from flask_cors import CORS
+# Enable CORS for your app, which can be customized with parameters
+CORS(app)
 if __name__ == '__main__':
     app.run(debug=True)
