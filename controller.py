98d06d3039afaa2913e292b46616ca2738d730c1
Shou-Tzu Han <debra4117@gmail.com>
2024-02-29 21:07:20 -0600

    update

--- controller.py
+++ controller.py
@@ -1,16 +1,16 @@
-from flask import Flask, jsonify, request  # Make sure to import 'request' here
+from flask import Flask, jsonify, request
 import google_sheets_service
 import os
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS
 
 SPREADSHEET_ID = os.getenv('GOOGLE_SPREADSHEET_ID')
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for the app
+CORS(app)
 
 @app.route('/data', methods=['GET'])
 def get_data():
-    range_name = 'Sheet1'  # Specify the actual range
+    range_name = 'Sheet1'
     data = google_sheets_service.get_sheet_data(range_name)
     return jsonify(data)
 
@@ -19,24 +19,22 @@ def update_headers():
     headers = request.json.get('headers', [])
     if not headers:
         return jsonify({"error": "No headers provided"}), 400
-    range_name = 'A1:J1'  # Update this range as needed
+    range_name = 'A1:J1'
     result = google_sheets_service.update_sheet_headers(SPREADSHEET_ID, range_name, headers)
     return jsonify({"message": "Headers updated successfully", "updatedCells": result.get('updatedCells')})
-
 
 @app.route('/add-row', methods=['POST'])
 def add_row():
     data = request.json
     values = data.get('values')
-    range_name = 'A1:J1'  # Specify the range or sheet name as needed
+    range_name = 'A1:J1'
     result = google_sheets_service.add_row(SPREADSHEET_ID, range_name, values)
     return jsonify(result)
-
 
 @app.route('/update-values', methods=['POST'])
 def update_values():
     data = request.json
-    range_name = data.get('range')  # Corrected from data.get('A1:J1')
+    range_name = data.get('range')
     values = data.get('values')
     if not range_name or not values:
         return jsonify({"error": "Missing range or values"}), 400
@@ -46,12 +44,11 @@ def update_values():
 @app.route('/clear-values', methods=['POST'])
 def clear_values():
     data = request.json
-    range_name = data.get('range')  # Corrected from data.get('A1:J1')
+    range_name = data.get('range')
     if not range_name:
         return jsonify({"error": "Missing range"}), 400
     result = google_sheets_service.clear_values(SPREADSHEET_ID, range_name)
     return jsonify(result)
-
 
 @app.route('/search', methods=['GET'])
 def search():
@@ -64,7 +61,3 @@ def search():
         return jsonify(row_data)
     else:
         return jsonify({"message": "No match found"}), 404
-
-
-
-
